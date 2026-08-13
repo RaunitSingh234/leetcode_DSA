0e29dfa471dfe27a979dfed09f7739683d5e02d5
Raunit Raj Singh <160695538+RaunitSingh234@users.noreply.github.com>
2026-08-13 11:35:46 +0530

    Time: 44 ms (27.43%), Space: 123.9 MB (53.54%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.java
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.java
@@ -2,32 +2,19 @@ class Solution {
     public int countDays(int days, int[][] meetings) {
         Arrays.sort(meetings, Comparator.comparingInt(a -> a[0]));
 
-        List<int[]> res = new ArrayList<>();
+        int maxEnd = meetings[0][1];
 
-        res.add(meetings[0]);
+        int gap =0;
 
-        for(int i =1; i<meetings.length; i++){
-            if(meetings[i][0] <= res.get(res.size()-1)[1]){
-                res.get(res.size() -1 )[0]=Math.min(
-                    res.get(res.size() - 1)[0],
-                    meetings[i][0]
-                );
-                res.get(res.size() -1 )[1]=Math.max(
-                    res.get(res.size() - 1)[1],
-                    meetings[i][1]
-                );
-            } else {
-                res.add(meetings[i]);
+        for(int i = 1; i<meetings.length; i++){
+            if (meetings[i][0] > maxEnd) {
+                gap = gap + meetings[i][0] - maxEnd - 1;
             }
-        }
 
-        int gap = 0;
-
-        for(int i = 1; i<res.size(); i++){
-            gap = gap + (res.get(i)[0] - res.get(i - 1)[1]) - 1;
+            maxEnd = Math.max(maxEnd, meetings[i][1]);
         }
-        gap = gap + res.get(0)[0] - 1;
-        gap = gap + days - res.get(res.size() - 1)[1];
+        gap = gap + meetings[0][0] - 1;
+        gap = gap + days - maxEnd;
 
         return gap;
     }
